6f1350567434cb42b056cad28dcd122f79f4add4
AkutoBlade <rared.isaacs@gmail.com>
2022-08-15 09:31:45 +0200

    Update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
 );
 
 app.use(cors({
-    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    origin: ['http://127.0.0.1:8080', 'http://localhost:8080'],
     credentials: true
  }));
 
